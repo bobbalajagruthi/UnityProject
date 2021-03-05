e46c80d8f524162d966a63acec5731d2eb9a484a
bobbalajagruthi <60417998+bobbalajagruthi@users.noreply.github.com>
2021-03-04 20:51:07 -0600

    commit
    commit

---
+++ FinalProject/Assets/Scripts/CameraPosition.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPosition : MonoBehaviour
+{
+    // Creating Game Object for player
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position +new Vector3(0, 20 ,-10);
+    }
+}
---
+++ FinalProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Declaring Variables
+
+    public float speed = 4.0f;
+    public float jumpSpeed = 20.0f;
+    public bool isOnGround = true;
+    public ParticleSystem crateExplosion;
+
+
+    private Rigidbody playerBallRb;
+    private float gravityModifier = 1.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerBallRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardMovement = Input.GetAxis("Vertical");
+        float sideMovement = Input.GetAxis("Horizontal");
+        playerBallRb.AddForce(Vector3.forward * speed * forwardMovement);
+        playerBallRb.AddForce(Vector3.right * speed * sideMovement);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround) 
+        {
+            playerBallRb.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+        else if (collision.gameObject.CompareTag("Cube"))
+        {
+            crateExplosion.Play();
+        }
+    }
+}
